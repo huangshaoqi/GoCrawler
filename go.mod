2643c7e7e6876cc53bbe70c264028263152c4de4
huangshaoqi <290678391@qq.com>
2021-01-09 18:06:44 +0800

    添加elasticsearch,讲Item存进elasticsearch

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module GoCrawler
 go 1.15
 
 require (
+	github.com/mailru/easyjson v0.7.6 // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	golang.org/x/text v0.3.4
+	gopkg.in/olivere/elastic.v5 v5.0.86
 )
--- persist/itemsaver.go
+++ persist/itemsaver.go
@@ -1,8 +1,41 @@
 package persist
 
+import (
+	"context"
+	"gopkg.in/olivere/elastic.v5"
+	"log"
+)
+
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
-	go func() {}()
+	go func() {
+		itemCount := 0
+		for {
+			item := <-out
+			//log.Printf("Item Saver: got item #%d: %v", itemCount, item)
+			itemCount++
+			_, err := save(item)
+			if err != nil {
+				log.Printf("Item Saver:error saving item %v:%v", item, err)
+			}
+		}
+	}()
 	return out
+
+}
+
+func save(item interface{}) (string, error) {
+	client, err := elastic.NewClient(
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Index().Index("dating_profile").Type("ygdy8").BodyJson(item).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	return response.Id, nil
 
 }
